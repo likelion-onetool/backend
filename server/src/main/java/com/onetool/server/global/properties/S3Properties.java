bfc66ab4d036efeb7cf1030389ef064cdbf31de9
LeeDongHoon <likelion.leedh@gmail.com>
2025-01-20 15:17:48 +0900

    [feat] S3 설정 및 속성 파일 생성

--- server/src/main/java/com/onetool/server/global/properties/S3Properties.java
+++ server/src/main/java/com/onetool/server/global/properties/S3Properties.java
@@ -1,24 +1,28 @@
 package com.onetool.server.global.properties;
 
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@Getter
 public class S3Properties {
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
 
-    @Value("${cloud.aws.s3.bucket.inspection-directory}")
-    private String bucketInspectionDirectory;
+    public static String BUCKET_NAME;
+    public static String BUCKET_INSPECTION_DIRECTORY;
+    public static String BUCKET_DETAILS_DIRECTORY;
+    public static String BUCKET_THUMBNAIL_DIRECTORY;
+    public static String BUCKET_SALES_DIRECTORY;
 
-    @Value("${cloud.aws.s3.bucket.details-directory}")
-    private String bucketDetailsDirectory;
-
-    @Value("${cloud.aws.s3.bucket.thumbnail-directory}")
-    private String bucketThumbnailDirectory;
-
-    @Value("${cloud.aws.s3.bucket.sales-directory}")
-    private String bucketSalesDirectory;
+    public S3Properties(
+            @Value("${cloud.aws.s3.bucket}") String bucketName,
+            @Value("${cloud.aws.s3.bucket.inspection-directory}") String bucketInspectionDirectory,
+            @Value("${cloud.aws.s3.bucket.details-directory}") String bucketDetailsDirectory,
+            @Value("${cloud.aws.s3.bucket.thumbnail-directory}") String bucketThumbnailDirectory,
+            @Value("${cloud.aws.s3.bucket.sales-directory}") String bucketSalesDirectory
+    ) {
+        BUCKET_NAME = bucketName;
+        BUCKET_INSPECTION_DIRECTORY = bucketInspectionDirectory;
+        BUCKET_DETAILS_DIRECTORY = bucketDetailsDirectory;
+        BUCKET_THUMBNAIL_DIRECTORY = bucketThumbnailDirectory;
+        BUCKET_SALES_DIRECTORY = bucketSalesDirectory;
+    }
 }
