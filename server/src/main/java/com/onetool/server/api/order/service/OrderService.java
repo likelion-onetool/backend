3db79bb3fa1cfb0f24db2c9c6e2a36c160e9b49f
PlusUltraCode <kkd06144@naver.com>
2025-03-19 10:45:32 +0900

    feat(Orders): orders N+1문제 해결 1010 쿼리 -> 13개 쿼리

--- server/src/main/java/com/onetool/server/api/order/service/OrderService.java
+++ server/src/main/java/com/onetool/server/api/order/service/OrderService.java
@@ -4,9 +4,7 @@ import com.onetool.server.api.blueprint.Blueprint;
 import com.onetool.server.api.order.OrderBlueprint;
 import com.onetool.server.api.order.Orders;
 import com.onetool.server.api.order.repository.OrderRepository;
-import com.onetool.server.global.exception.OrderNotFoundException;
 import com.onetool.server.api.member.domain.Member;
-import com.onetool.server.global.exception.OrdersNullPointException;
 import com.onetool.server.global.new_exception.exception.ApiException;
 import com.onetool.server.global.new_exception.exception.error.OrderErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -25,13 +23,18 @@ public class OrderService {
 
     @Transactional
     public List<Orders> findAllOrdersByUserId(Long memberId) {
-        return orderRepository.findByUserId(memberId);
+        return orderRepository.findByMemberId(memberId);
     }
 
     @Transactional
     public Orders findOrdersById(Long orderId) {
         return orderRepository.findById(orderId)
                 .orElseThrow(() -> new ApiException(OrderErrorCode.NOT_FOUND_ERROR,"orderId : "+orderId));
+    }
+
+    public List<Object[]> CountOrderItems(List<Long> orderIds){
+        List<Object[]> objects = orderRepository.countOrderItemsForOrders(orderIds);
+        return objects;
     }
 
     @Transactional
@@ -51,7 +54,7 @@ public class OrderService {
 
     @Transactional(readOnly = true)
     public List<Orders> findAllByUserId(Long userId) {
-        return orderRepository.findByUserId(userId);
+        return orderRepository.findByMemberId(userId);
     }
 
     private void validateOrdersIsNull(Orders orders) {
