3b4616bdf6f5e67e6f7dd5a2587dffe5a88cbfa7
mete0rfish <sungwon326@naver.com>
2025-08-12 17:20:20 +0900

    chore: Postgres로 변경

--- server/src/main/java/com/onetool/server/api/chat/repository/ChatMysqlSpringImpl.java
+++ server/src/main/java/com/onetool/server/api/chat/repository/ChatMysqlSpringImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Primary
 @Repository
 @RequiredArgsConstructor
 public class ChatMysqlSpringImpl implements ChatRepository {
