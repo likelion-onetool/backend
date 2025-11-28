8b24d6a4326ee7ff8d4435a09b30be8f2d469442
mete0rfish <sungwon326@naver.com>
2025-11-28 10:38:06 +0900

    chore: RabbitMQ 삭제

--- server/src/main/java/com/onetool/server/api/chat/service/mq/ChatProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.onetool.server.api.chat.service.mq;
-
-import com.onetool.server.api.chat.domain.ChatMessage;
-
-public interface ChatProducerService {
-
-    void sendMessage(ChatMessage messageDTO);
-}
